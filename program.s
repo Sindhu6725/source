df9f6069bfe00fa8a6ea44102099619777476615
Sindhu6725 <sindhu.nm@s.amity.edu>
2025-03-29 20:51:40 +0530

    mini calculator

---
+++ program.s
@@ -0,0 +1,18 @@
+a=5
+b=6.0
+op=int(intput("enter 1 for + 2 for - 3 for * 4 for /"))
+if(op==1):
+c=a+b
+print(c)
+elif(op==2):
+c=a-b
+print(c)
+elif(op==3):
+c=a*b
+print(c)
+elif(op=4):
+c=a/b
+print(c)
+else:
+print("b should be greater than 1")
+
